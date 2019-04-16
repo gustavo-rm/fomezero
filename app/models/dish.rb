211138fdfb04dd224cd48a6a9ed1652f6982f745
Gustavo <gustavomalacarne@alunos.utfpr.edu.br>
2019-04-15 21:34:50 -0300

    Versão 2.1 - Relacionamento entre Tabelas

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,2 +1,4 @@
 class Dish < ApplicationRecord
+	belongs_to :restaurant
+  	has_and_belongs_to_many :ingredients
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_001327) do
+ActiveRecord::Schema.define(version: 2019_04_16_001753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2019_04_16_001327) do
     t.float "preparationtime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "restaurant_id"
+    t.index ["restaurant_id"], name: "index_dishes_on_restaurant_id"
   end
 
   create_table "ingredients", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema.define(version: 2019_04_16_001327) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "dishes", "restaurants"
 end
