f000f7c3042c043e64e82287bb4a812b3f8624b5
Gustavo Malacarne <gustavomalacarne@alunos.utfpr.edu.br>
2019-04-21 18:37:53 -0300

    Versão 6 - Bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 	def menu(description, path)
-		content_tag(:li) do
-			link_to description, path
+		content_tag(:li, class:"nav-item") do
+			link_to description, path, class:"nav-link"
 		end
 	end
 end
